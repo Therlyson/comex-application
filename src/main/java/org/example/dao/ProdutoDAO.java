4c6dde7d3553c56ebcf55248c7359b93639b55c2
Therlyson Ryan <therlysonsousa@gmail.com>
2024-06-16 14:20:30 -0300

    Criando o crud de produto

---
+++ src/main/java/org/example/dao/ProdutoDAO.java
@@ -0,0 +1,127 @@
+package org.example.dao;
+
+import org.example.model.Produto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+    private Connection connection;
+
+    public ProdutoDAO(Connection connection){
+        this.connection = connection;
+    }
+
+    public Boolean cadastrarProduto(Produto produto) {
+        String sql = "INSERT INTO produto(nome, descricao, preco) VALUES (?, ?, ?)";
+
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1, produto.getNome());
+            ps.setString(2, produto.getDescricao());
+            ps.setDouble(3, produto.getPreco());
+
+            int resultado = ps.executeUpdate();
+            return resultado==1;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Produto> listarProdutos(){
+        String sql = "SELECT * FROM produto";
+        List<Produto> produtos = new ArrayList<>();
+
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Produto produto = resultProduto(rs);
+                produtos.add(produto);
+            }
+
+            rs.close();
+            return produtos;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Produto resultProduto(ResultSet rs) throws SQLException {
+        Long id = rs.getLong("id");
+        String nome = rs.getString("nome");
+        String descricao = rs.getString("descricao");
+        Double preco = rs.getDouble("preco");
+        return new Produto(id, nome, descricao, preco);
+    }
+
+    public List<String> consultarProdutosPorNome(){
+        String sql = "SELECT nome FROM produto ORDER BY nome";
+        List<String> produtos = new ArrayList<>();
+
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                String nome = rs.getString("nome");
+                produtos.add(nome);
+            }
+
+            rs.close();
+            return produtos;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Produto buscarProduto(Long id){
+        String sql = "SELECT * FROM produto WHERE id = " +id;
+
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Produto produto = resultProduto(rs);
+                rs.close();
+                return produto;
+            }
+            return null;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public Produto alterarProduto(Produto produto){
+        String sql = "UPDATE produto SET nome=?, descricao=?, preco=? WHERE id=?";
+
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+
+            ps.setString(1, produto.getNome());
+            ps.setString(2, produto.getDescricao());
+            ps.setDouble(3, produto.getPreco());
+            ps.setLong(4, produto.getId());
+
+            ps.execute();
+            return produto;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Boolean remover(Long id) {
+        String sql = "DELETE FROM produto WHERE id = ?";
+
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+
+            ps.setLong(1, id);
+
+            int resultado = ps.executeUpdate();
+            return resultado==1;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/org/example/services/ProdutoService.java
@@ -0,0 +1,48 @@
+package org.example.services;
+
+import org.example.conexao.ConnectionFactory;
+import org.example.dao.ProdutoDAO;
+import org.example.model.Produto;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class ProdutoService {
+    private ConnectionFactory factory;
+
+    public ProdutoService(){
+        factory = new ConnectionFactory();
+    }
+
+    private ProdutoDAO produtoDAO(){
+        Connection connection = factory.criaConexao();
+        return new ProdutoDAO(connection);
+    }
+
+    public Boolean cadastrarProduto(Produto produto){
+        return produtoDAO().cadastrarProduto(produto);
+    }
+
+    public List<Produto> listarProdutos(){
+        return produtoDAO().listarProdutos();
+    }
+
+    public List<String> consultarProdutosPorNome(){
+        return produtoDAO().consultarProdutosPorNome();
+    }
+
+    public Produto alterarProduto(Long id, String nome, String descricao, Double preco){
+        Produto produto = produtoDAO().buscarProduto(id);
+        if(produto != null){
+            produto.setNome(nome);
+            produto.setDescricao(descricao);
+            produto.setPreco(preco);
+            return produtoDAO().alterarProduto(produto);
+        }
+        return null;
+    }
+
+    public Boolean removerProduto(Long id){
+        return produtoDAO().remover(id);
+    }
+}
